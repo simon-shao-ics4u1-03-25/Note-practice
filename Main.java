753f68e846481f547eda4c7931e7f945a26ad059
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-02-04 19:33:35 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
-Class Notes {
-  public static void main(String[] args]{
-    System.out.println["Hellow World");//outputs then output a new line
+class Main {
+  public static void main(String[] args){
+    System.out.println("Hellow World");//outputs then output a new line
+
+//lowercase first letter data type are primitive, uppercase are non-primitiv- can be broken down into other data types
+//escape character "\"
+// \n, new line character, \t tab, \" -changes from being a string ending delimiter to a normal character
+// \\ change escape character to normal backslash
+double fracNum;
+fracNum = 3.5;
+fracNum = 4.0;// 4.0!=4
+//subtraction, (-), multiplication (*), division (/), Mod (%) - remainder of division
+System.out.println(fracNum);
+//Variable call - use, when variable is not on left side of an assignment, replace the variable with a copy of value
+
   }
+
 }
